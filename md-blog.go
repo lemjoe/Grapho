eac43e5e7f33b6e29aee23c94a183f04f0b3d58b
Alexander Ignatov <3787010@gmail.com>
2023-10-10 21:59:53 +0300

    added upload/delete functions

--- md-blog.go
+++ md-blog.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"io"
+	"time"
+
 	"github.com/gomarkdown/markdown"
 
 	"html/template"
@@ -25,6 +28,9 @@ func init() {
 func main() {
 	http.HandleFunc("/show", ShowArticle)
 	http.HandleFunc("/edit", Editor)
+	http.HandleFunc("/delete", DeleteArticle)
+	http.HandleFunc("/add", UploadArticle)
+	http.HandleFunc("/upload", Upload)
 	http.HandleFunc("/", ArticleList)
 	http.HandleFunc("/callme", CallMe)
 	log.Print("Server is running on port 3608")
@@ -82,6 +88,74 @@ func Editor(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func DeleteArticle(w http.ResponseWriter, r *http.Request) {
+
+	artclPath := r.URL.Query().Get("md")
+	err := os.Remove("articles/" + artclPath)
+	if err != nil {
+		log.Print("MD file delete error: ", err)
+	}
+	w.Write([]byte("Successfully Deleted File\n"))
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+}
+
+func UploadArticle(w http.ResponseWriter, r *http.Request) {
+
+	t, err := template.ParseFiles("upload.html") //parse the html file homepage.html
+	if err != nil {                              // if there is an error
+		log.Print("template parsing error: ", err) // log it
+	}
+	err = t.Execute(w, time.Now()) //execute the template and pass it the HomePageVars struct to fill in the gaps
+	if err != nil {                // if there is an error
+		log.Print("template executing error: ", err) //log it
+	}
+}
+
+func Upload(w http.ResponseWriter, r *http.Request) {
+	log.Println("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	r.ParseMultipartForm(10 << 20)
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		log.Println("Error Retrieving the File")
+		log.Println(err)
+		return
+	}
+	if handler.Header.Get("Content-Type") != "text/markdown" {
+		log.Println("File type must be text/markdown")
+		return
+	}
+	defer file.Close()
+	log.Printf("Uploaded File: %+v\n", handler.Filename)
+	log.Printf("File Size: %+v\n", handler.Size)
+	log.Printf("MIME Header: %+v\n", handler.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+
+	tempFile, err := os.Create("articles/" + handler.Filename)
+	if err != nil {
+		log.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := io.ReadAll(file)
+	if err != nil {
+		log.Println(err)
+	}
+	// write this byte array to our temporary file
+	tempFile.Write(fileBytes)
+	// return that we have successfully uploaded our file!
+	w.Write([]byte("Successfully Uploaded File\n"))
+}
+
 func ArticleList(w http.ResponseWriter, r *http.Request) {
 
 	f, err := os.Open("articles")
@@ -100,7 +174,7 @@ func ArticleList(w http.ResponseWriter, r *http.Request) {
 
 	for _, v := range files {
 		if !v.IsDir() {
-			html += "<li>" + "<a href='show?md=" + v.Name() + "'>" + v.Name() + "</a><i> (Last modification: " + v.ModTime().Format("2006-Jan-02") + ") </i><a href='edit?md=" + v.Name() + "'><i>edit</i></a></li>"
+			html += "<li>" + "<a href='show?md=" + v.Name() + "'>" + v.Name() + "</a><i> (Last modification: " + v.ModTime().Format("2006-Jan-02") + ") </i><a href='edit?md=" + v.Name() + "'><i>edit</i></a> | <a href='delete?md=" + v.Name() + "'><i>delete</i></a></li>"
 		}
 	}
 

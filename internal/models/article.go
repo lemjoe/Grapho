19c001793b60c72c20c845fcc622ca50059bb088
mallvielfrass <jack@bykollab.com>
2023-10-21 01:07:14 +0300

    add models

---
+++ internal/models/article.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Article struct {
+	FileName         string    `json:"file_name"`
+	Title            string    `json:"article_title"`
+	Author           string    `json:"article_author"`
+	CreationDate     time.Time `json:"creation_date"`
+	ModificationDate time.Time `json:"modification_date"`
+	IsLocked         bool      `json:"is_locked"`
+}

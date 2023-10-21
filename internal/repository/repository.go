c985b2ea4677cc4578a8ebb01f8f3335d7c12788
mallvielfrass <jack@bykollab.com>
2023-10-21 11:04:45 +0300

    add repo article methods

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,14 +1,24 @@
 package repository
 
 import (
+	"github.com/lemjoe/md-blog/internal/models"
+	"github.com/lemjoe/md-blog/internal/repository/cloverdb/article"
 	"github.com/lemjoe/md-blog/internal/repository/cloverdb/user"
 	"github.com/ostafen/clover/v2"
 )
 
 type User interface {
 }
+type Article interface {
+	CreateArticle(article models.Article) (models.Article, error)
+	GetAllArticles() ([]models.Article, error) //todo add pagination
+	GetArticleByFileName(fileName string) (models.Article, error)
+	DeleteArticleByFileName(fileName string) error
+	UpdateArticleByFileName(fileName string) error
+}
 type Repository struct {
-	User User
+	User    User
+	Article Article
 }
 
 func NewRepository(db *clover.DB) (*Repository, error) {
@@ -16,7 +26,12 @@ func NewRepository(db *clover.DB) (*Repository, error) {
 	if err != nil {
 		return nil, err
 	}
+	article, err := article.Init(db)
+	if err != nil {
+		return nil, err
+	}
 	return &Repository{
-		User: user,
+		User:    user,
+		Article: article,
 	}, nil
 }

876997d5af544f9797df029fa5c24a91482ff79c
mallvielfrass <jack@bykollab.com>
2023-10-22 21:54:20 +0300

    init deprecated handler

---
+++ internal/handler/deprecated/types.go
@@ -0,0 +1,15 @@
+package deprecated
+
+import "html/template"
+
+type PageVariables struct {
+	Md           string
+	MDArticle    template.HTML
+	HomeButton   string
+	AddButton    string
+	Title        string
+	Path         string
+	Author       string
+	CreationDate string
+	UpdateDate   string
+}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,16 +4,20 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/lemjoe/md-blog/internal/handler/deprecated"
 	"github.com/lemjoe/md-blog/internal/service"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
 )
 
 type Handler struct {
 	services *service.Service
+	bundle   *i18n.Bundle
 }
 
-func NewHandler(services *service.Service) *Handler {
+func NewHandler(services *service.Service, bundle *i18n.Bundle) *Handler {
 	return &Handler{
 		services: services,
+		bundle:   bundle,
 	}
 }
 
@@ -29,7 +33,9 @@ func (h *Handler) Run(port string) error {
 	// http.HandleFunc("/singup", SingUp)
 	// http.HandleFunc("/singin", SingIn)
 	// http.HandleFunc("/", ArticleList)
+	http.Handle("/", deprecated.Init(h.bundle, h.services).Router())
 	http.Handle("/lib/", http.StripPrefix("/lib/", http.FileServer(http.Dir("lib"))))
+
 	http.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images"))))
 	log.Print("Server is running on port ", port)
 	return http.ListenAndServe(port, nil)

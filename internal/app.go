e32de2ba8c23c03341691d8c18b53d6143253b7f
mallvielfrass <jack@bykollab.com>
2023-10-22 22:22:59 +0300

    add i18n

--- internal/app.go
+++ internal/app.go
@@ -5,6 +5,8 @@ import (
 	"github.com/lemjoe/md-blog/internal/repository"
 	"github.com/lemjoe/md-blog/internal/repository/cloverdb"
 	"github.com/lemjoe/md-blog/internal/service"
+	"github.com/nicksnyder/go-i18n/v2/i18n"
+	"golang.org/x/text/language"
 )
 
 type App struct {
@@ -23,8 +25,9 @@ func (a *App) Run() error {
 	if err != nil {
 		return err
 	}
+	bundle := i18n.NewBundle(language.English)
 	services := service.NewService(repos)
-	handlers := handler.NewHandler(services)
+	handlers := handler.NewHandler(services, bundle)
 	err = handlers.Run(":4007")
 	return err
 }
